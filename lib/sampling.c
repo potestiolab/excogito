48102e05cbf1a2d6c4f1d789f891afa7f0928cb5
RiccardoAldrigo <107126715+RiccardoAldrigo@users.noreply.github.com>
2023-04-21 15:11:22 +0200

    Update sampling.c

--- lib/sampling.c
+++ lib/sampling.c
@@ -11,6 +11,7 @@
 #include <alignment.h>
 #include <mapping.h>
 #include <observables.h>
+#include <time.h>
 
 void my_make_a_move(cg_mapping *old_mapping, cg_mapping *new_mapping, int rem_add[2]) {
     /**
@@ -52,7 +53,7 @@ void my_make_a_move(cg_mapping *old_mapping, cg_mapping *new_mapping, int rem_ad
     }
 }
 
-void accept_move(int rem_add[2], cg_mapping *mapping, cg_mapping *new_mapping, alignments *align, alignments *new_align, traj *Trajectory, clust_params *clustering, double **new_coefficients_matrix){
+void accept_move(int rem_add[2], cg_mapping *mapping, cg_mapping *new_mapping, traj *Trajectory){
     /**
     * routine that accepts a Simulated Annealing move. It updates all the relevant observables.
     *
@@ -71,13 +72,14 @@ void accept_move(int rem_add[2], cg_mapping *mapping, cg_mapping *new_mapping, a
     mapping->mapping[rem_add[0]] = 0;
     mapping->mapping[rem_add[1]] = 1;
     mapping->smap = new_mapping->smap;
-    for (dummy = 0; dummy < Trajectory->pairs; dummy++) { align->rmsd_mat[dummy] = new_align->rmsd_mat[dummy]; }
+    mapping->res = new_mapping->res;
+    /*for (dummy = 0; dummy < Trajectory->pairs; dummy++) { align->rmsd_mat[dummy] = new_align->rmsd_mat[dummy]; }
     if (clustering->crit==3){
         for (dummy = 0; dummy < Trajectory->frames*2; dummy++){align->rmsd_vector[dummy] = new_align->rmsd_vector[dummy];}
-    }   
+    }   */										//(!)
 }
 
-double tzero_estimation(traj *Trajectory, clust_params *clustering, int cgnum, int rsd, int verbose, int kl_flag, FILE *f_out_l) {
+double tzero_estimation(traj *Trajectory, int cgnum, int rsd, int verbose, int kl_flag, FILE *f_out_l) {		//(!)
     /**
     * routine that makes *nrun* unbiased moves. It is used to estimate the starting temperature for Simulated Annealing.
     *
@@ -96,15 +98,6 @@ double tzero_estimation(traj *Trajectory, clust_params *clustering, int cgnum, i
     deltas = d1t(20); // fixed number of moves generated
     int dummy, nr;
     int rem_add[2]; // vector containing removed and added atom
-    // alignment object
-    alignments *align = malloc (sizeof(alignments));
-    align->rmsd_mat = d1t(Trajectory->pairs);
-    align->rotation_matrices = d2t(Trajectory->pairs, 9);
-    align->coms = d2t(Trajectory->frames, 3);
-    align->rsd = rsd;
-    align->rmsd_vector = d1t(Trajectory->frames * 2);
-    align->rotation_matrices_vector = d2t(Trajectory->frames * 2, 9);
-    double **new_coefficients_matrix;
     // mapping
     cg_mapping *mapping = malloc (sizeof(cg_mapping));
     mapping->n_at = Trajectory->n_at;
@@ -113,6 +106,8 @@ double tzero_estimation(traj *Trajectory, clust_params *clustering, int cgnum, i
     mapping->smap = -1.0;
     mapping->clusters = i1t(Trajectory->frames);
     mapping->size = i1t(Trajectory->frames);
+    mapping->idx_cluster = i1t(Trajectory->frames); //ADDED				//(!)
+    mapping->omega = i1t(Trajectory->frames);//ADDED					//(!)
     cg_mapping *new_mapping = malloc(sizeof(cg_mapping));
     new_mapping->n_at = Trajectory->n_at;
     new_mapping->n_cg = cgnum;
@@ -120,18 +115,15 @@ double tzero_estimation(traj *Trajectory, clust_params *clustering, int cgnum, i
     new_mapping->mapping = i1t(Trajectory->n_at);
     new_mapping->clusters = i1t(Trajectory->frames);
     new_mapping->size = i1t(Trajectory->frames);
+    new_mapping->idx_cluster = i1t(Trajectory->frames);//ADDED				//(!)
+    new_mapping->omega = i1t(Trajectory->frames);//ADDED				//(!)
     int d_idx = 0;
     generate_random_mapping(mapping,f_out_l);
-    if (clustering->crit==3){cycle_alignment_fastclust(Trajectory, align, mapping);}
-    else{cycle_alignment(Trajectory, align, mapping);}
-    overall_compute_smap(align, clustering, Trajectory, mapping, verbose, kl_flag);
+    clustering_and_smap(Trajectory, mapping); //ADDED			//(!)
     for (nr = 0; nr < 20; nr++) {
         my_make_a_move(mapping, new_mapping, rem_add);
         convert_mapping(new_mapping, f_out_l);
-        if (clustering->crit==3){correct_rmsd_fastclust(align, Trajectory, align, mapping->n_cg, rem_add[0],rem_add[1]);}
-        else{correct_rmsd(align, Trajectory, align, mapping->n_cg, rem_add[0],rem_add[1]);}
-        // compute new smap
-        overall_compute_smap(align, clustering, Trajectory, new_mapping, verbose, kl_flag);
+        clustering_and_smap(Trajectory, new_mapping); //ADDED			//(!)
         // add delta
         deltas[d_idx] = fabs(new_mapping->smap - mapping->smap);
         // updating mapping with new smap and new site
@@ -152,12 +144,14 @@ double tzero_estimation(traj *Trajectory, clust_params *clustering, int cgnum, i
     double corr_t_zero = -avg_delta/(log(0.75));
     free_mapping(mapping);
     free_mapping(new_mapping);
-    free_alignment(align);
     free_d1t(deltas);
+
+    //corr_t_zero = 0.7;								//(!!!)
+    
     return corr_t_zero;
 }
 
-void simulated_annealing(traj *Trajectory, clust_params *clustering, MC_params *SA_params, int cgnum, int rsd, int verbose, int kl_flag, FILE *f_out_l){
+void simulated_annealing(traj *Trajectory, MC_params *SA_params, int cgnum, int rsd, int verbose, int kl_flag, FILE *f_out_l, int core){
     /**
     * simulated annealing optimisation
     * Parameters
@@ -188,6 +182,20 @@ void simulated_annealing(traj *Trajectory, clust_params *clustering, MC_params *
         printf("MC_steps*frames (%d) > 10^6: setting verbose to 0 to avoid wasting too much disk space\n", SA_params->MC_steps*Trajectory->frames);
         verbose = 0;
     }
+
+    char out_filename[1000];   
+    /*                                         
+    sprintf(out_filename, "./MC_CONVERGENCE/MC_converge_N%d_Ncg%d_MC%d_c%d.csv", Trajectory->n_at, cgnum, SA_params->MC_steps, core);
+    FILE* f_smap_mc;
+    f_smap_mc = open_file_w(out_filename);
+    fprintf(f_smap_mc, "Smap\n");
+    */
+
+    sprintf(out_filename, "./MC_RESOLUTION/MC_resolution_N%d_Ncg%d_MC%d_c%d.csv", Trajectory->n_at, cgnum, SA_params->MC_steps, core);	//(!)
+    FILE* f_res_mc;
+    f_res_mc = open_file_w(out_filename);
+    fprintf(f_res_mc, "Resolution\n");
+
     cg_mapping *mapping = malloc (sizeof(cg_mapping));
     cg_mapping *new_mapping = malloc (sizeof(cg_mapping));
     cg_mapping *lowest_mapping = malloc (sizeof(cg_mapping));
@@ -198,52 +206,45 @@ void simulated_annealing(traj *Trajectory, clust_params *clustering, MC_params *
     int dummy;
     int rem_add[2];
     double **new_coefficients_matrix;
-    //new_rmsd_mat = d1t(Trajectory->pairs);
+    time_t seconds;
+    time_t seconds_ref = time(NULL);
     // MC
     double r, p;
-    alignments *align = malloc (sizeof(alignments));
-    align->rmsd_mat = d1t(Trajectory->pairs);
-    align->rotation_matrices = d2t(Trajectory->pairs, 9);
-    align->coms = d2t(Trajectory->frames, 3);
-    align->rsd = rsd;
-    align->rmsd_vector = d1t(Trajectory->frames * 2);
-    align->rotation_matrices_vector = d2t(Trajectory->frames * 2, 9);
-    // new alignment
-    alignments *new_align =  malloc (sizeof(alignments));
-    new_align->rmsd_mat = d1t(Trajectory->pairs);
-    new_align->rmsd_vector = d1t(Trajectory->frames * 2);
     // mapping
     mapping->n_at = Trajectory->n_at;
     mapping->n_cg = cgnum;
     mapping->mapping = i1t(Trajectory->n_at);
     mapping->smap = -1.0;
+    mapping->res  = -1.0;
     mapping->clusters = i1t(Trajectory->frames);
     mapping->size = i1t(Trajectory->frames);
+    mapping->idx_cluster = i1t(Trajectory->frames); //ADDED				//(!)
+    mapping->omega = i1t(Trajectory->frames);//ADDED					//(!)
     new_mapping->n_at = mapping->n_at;
     new_mapping->n_cg = cgnum;
     new_mapping->mapping = i1t(mapping->n_at);
     new_mapping->clusters = i1t(Trajectory->frames);
     new_mapping->size = i1t(Trajectory->frames);
     new_mapping->smap = -1.0;
+    new_mapping->res  = -1.0;
+    lowest_mapping->res = -1.0;								//(!)
+    new_mapping->idx_cluster = i1t(Trajectory->frames);//ADDED				//(!)
+    new_mapping->omega = i1t(Trajectory->frames);//ADDED				//(!)
     lowest_mapping->n_at = mapping->n_at;
     lowest_mapping->n_cg = cgnum;
     lowest_mapping->mapping = i1t(mapping->n_at);
     lowest_mapping->clusters = i1t(Trajectory->frames);
     lowest_mapping->size = i1t(Trajectory->frames);
     lowest_mapping->smap = -1.0;
+    lowest_mapping->idx_cluster = i1t(Trajectory->frames);//ADDED			//(!)
+    lowest_mapping->omega = i1t(Trajectory->frames);//ADDED				//(!)
     printf("allocated variables\n");
     // random mapping and rotation matrices (and first rmsd_mat)
     printf("generating random mapping\n");
     generate_random_mapping(mapping, f_out_l);
     fprintf(f_out_l, "Initial mapping\n");
     convert_mapping(mapping, f_out_l);
-    // if fast clustering
-    if (clustering->crit==3){
-        cycle_alignment_fastclust(Trajectory, align, mapping);            
-    }
-    else{cycle_alignment(Trajectory, align, mapping);}
-    // calculate start mapping entropy
-    overall_compute_smap(align, clustering, Trajectory, mapping, verbose,kl_flag);
+    clustering_and_smap(Trajectory, mapping); //ADDED			//(!)
     fprintf(f_out_l, "start observable is %lf\n", mapping->smap);
     fprintf(f_out_l, "starting opt with T0 = %lf and decay_time %lf\n", SA_params->t_zero, SA_params->decay_time);
     update_mapping(mapping, lowest_mapping, Trajectory->frames);
@@ -256,25 +257,16 @@ void simulated_annealing(traj *Trajectory, clust_params *clustering, MC_params *
             fprintf(f_out_l, "mapping\n");
             convert_mapping(mapping, f_out_l);
         }
-        // check if we have to update rotation matrices and coms
-        if ((mc % SA_params->rotmats_period == 0) & (mc != 0) & clustering->crit != 4) {
-            fprintf(f_out_l, "Updating rotation matrices and coms\n");
-            if (clustering->crit==3){cycle_alignment_stride(Trajectory, align, mapping);}
-            else{cycle_alignment(Trajectory, align, mapping);}
-        }
         my_make_a_move(mapping, new_mapping, rem_add);
         fprintf(f_out_l, "move made: removed %d added %d\n", rem_add[0], rem_add[1]);
-        // new rmsd matrix
-        if (clustering->crit==3){
-            correct_rmsd_fastclust(new_align, Trajectory, align, mapping->n_cg, rem_add[0],rem_add[1]);}
-        else{correct_rmsd(new_align, Trajectory, align, mapping->n_cg, rem_add[0],rem_add[1]);}
         // observable
-        overall_compute_smap(new_align, clustering, Trajectory, new_mapping, verbose, kl_flag);
-        fprintf(f_out_l, "new_smap %lf\n", new_mapping->smap);
+        clustering_and_smap(Trajectory, new_mapping); //ADDED			//(!)
+        //fprintf(f_out_l, "new_smap %lf\n", new_mapping->smap);
+        
         // MC rule
         if ((new_mapping->smap - mapping->smap) < 0) {
             fprintf(f_out_l, "move accepted\n");
-            accept_move(rem_add,mapping,new_mapping,align,new_align,Trajectory,clustering,new_coefficients_matrix);
+            accept_move(rem_add,mapping,new_mapping,Trajectory);
             // check lowest
             if (mapping->smap < lowest_mapping->smap) {
                 fprintf(f_out_l, "Lowest mapping entropy found\n");
@@ -288,17 +280,42 @@ void simulated_annealing(traj *Trajectory, clust_params *clustering, MC_params *
             if (verbose == 1){fprintf(f_out_l, "r= %lf p = %lf\n", r, p);}
             if (p > r) {
                 fprintf(f_out_l, "p > r move accepted\n");
-                accept_move(rem_add,mapping,new_mapping,align,new_align,Trajectory,clustering,new_coefficients_matrix);
+                accept_move(rem_add,mapping,new_mapping,Trajectory);
                 }
             else {
                 fprintf(f_out_l, "move rejected\n");
             }
         }
         fprintf(f_out_l, "SMAP=%lf\n", mapping->smap);
+        
+        if (mc % 100 == 0 & mc != 0)
+        {
+            seconds = time(NULL);
+            printf("MC step number: %d\n", mc);
+            printf("time elapsed: %ld seconds\n", seconds - seconds_ref);
+        }
+
+	//fprintf(f_smap_mc, "%lf\n", lowest_mapping->smap);			//(!)
+        fprintf(f_res_mc, "%lf\n", mapping->res);                        	//(!)
+        //printf("\n RES = %lf\n", mapping->res);                        		//(!)
     }
     fprintf(f_out_l, "END OF SIMULATED ANNEALING OPTIMISATION\n");
     fprintf(f_out_l, "lowest_smap = %lf\n", lowest_mapping->smap);
     fprintf(f_out_l, "lowest_mapping = \n");
+    
+    // SAVING MINIMUM MAPPING                                            		
+    sprintf(out_filename, "./RESULTS/MappingMin_N%d_ncg%d_MC%d_c%d.dat", mapping->n_at, cgnum, SA_params->MC_steps, core);
+    FILE* f_smap;
+    f_smap = open_file_w(out_filename);
+    fprintf(f_smap, "n_at = %d\n", mapping->n_at);
+    fprintf(f_smap, "n_cg = %d\n", cgnum);
+    fprintf(f_smap, "MC steps = %d\n", SA_params->MC_steps);
+    fprintf(f_smap, "decay time = %lf\n", SA_params->decay_time);
+    fprintf(f_smap, "lowest_smap = %lf\n", lowest_mapping->smap);
+    fprintf(f_smap, "lowest_mapping =\n");                           
+    convert_mapping(lowest_mapping, f_smap);
+    fprintf(f_smap, "\nexecution_time = %ld\n", seconds - seconds_ref); 
+    
     for (dummy = 0; dummy < lowest_mapping->n_at; dummy++) { fprintf(f_out_l, "%d ", lowest_mapping->mapping[dummy]); }
     convert_mapping(lowest_mapping, f_out_l);
     fprintf(f_out_l, "last_smap %lf\n", mapping->smap);
@@ -309,6 +326,4 @@ void simulated_annealing(traj *Trajectory, clust_params *clustering, MC_params *
     free_mapping(mapping);
     free_mapping(new_mapping);
     free_mapping(lowest_mapping);
-    free_alignment(align);
-    free_new_alignment(new_align);
 }
