4e97cbd9047967b66339aa56abf3dcfa01b41dba
Marco Giulini <marco.giulini@unitn.it>
2023-08-04 10:20:59 +0200

    added test for optimize_spins

--- tests/test_suite.sh
+++ tests/test_suite.sh
@@ -26,9 +26,11 @@ declare -a HashTable=( "test0:$BUILDDIR/excogito optimize -p ../files/parameters
 "test21:$BUILDDIR/excogito norm -p ../files/parameters/parameters_norm_6d93_N31.ini -t ../files/trajectories/6d93_1000frames.xyz -e ../files/energies/6d93_energies_1000frames.txt -c 6d93 -m ../files/mappings/tamapin_ca_mapping.txt"
 "test22:$BUILDDIR/excogito distance -p ../files/parameters/parameters_distance_4ake_N214.ini -t ../files/trajectories/4ake_coord_xyz.xyz -e ../files/energies/4ake_energies_1frames.txt -c 4ake -x ../files/mappings/4ake_pdb_mappings.txt"
 "test23:$BUILDDIR/excogito measure -p ../files/parameters/parameters_measure_6d93_N31_fastclust.ini -t ../files/trajectories/6d93_1000frames.xyz -e ../files/energies/6d93_energies_1000frames.txt -c 6d93 -m ../files/mappings/tamapin_ca_mapping.txt"
-"test24:$BUILDDIR/excogito optimize_kl -p ../files/parameters/parameters_optimizekl_6d93_N31_notemp.ini -t ../files/trajectories/6d93_100frames.xyz -r ../files/energies/6d93_probs_100frames.txt -c 6d93"
-"test25:$BUILDDIR/excogito measure_kl -p ../files/parameters/parameters_measurekl_6d93_N31.ini -t ../files/trajectories/6d93_100frames.xyz -r ../files/energies/6d93_probs_100frames.txt -c 6d93 -m ../files/mappings/tamapin_ca_mapping.txt"
-"test26:$BUILDDIR/excogito random_kl -p ../files/parameters/parameters_randomkl_6d93_N31.ini -t ../files/trajectories/6d93_100frames.xyz -r ../files/energies/6d93_probs_100frames.txt -c 6d93")
+"test24:$BUILDDIR/excogito optimize_kl -p ../files/parameters/parameters_optimizekl_6d93_N31_notemp.ini -t ../files/trajectories/6d93_100frames.xyz -r ../files/probabilities/6d93_probs_100frames.txt -c 6d93"
+"test25:$BUILDDIR/excogito measure_kl -p ../files/parameters/parameters_measurekl_6d93_N31.ini -t ../files/trajectories/6d93_100frames.xyz -r ../files/probabilities/6d93_probs_100frames.txt -c 6d93 -m ../files/mappings/tamapin_ca_mapping.txt"
+"test26:$BUILDDIR/excogito random_kl -p ../files/parameters/parameters_randomkl_6d93_N31.ini -t ../files/trajectories/6d93_100frames.xyz -r ../files/probabilities/6d93_probs_100frames.txt -c 6d93"
+"test27:$BUILDDIR/excogito optimize_spins -p ../files/parameters/parameters_spins.ini -t ../files/trajectories/spins.csv -r ../files/probabilities/spins_probabilities.txt -c spin"
+)
 
 for test_line in "${HashTable[@]}"; do
 test_id="${test_line%%:*}"
